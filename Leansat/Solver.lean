9366651532ec707e3e5034637ae1128028146479
OperatorGK <operator.gk@gmail.com>
2021-06-20 17:07:38 +0300

    Refactoring

--- Leansat/Solver.lean
+++ Leansat/Solver.lean
@@ -4,12 +4,12 @@ import Leansat.CNF
 structure SolverState : Type where
   assignment : Assignment
   assignmentHistory : Array Nat
+  branchHistory : Array Nat
   cnf : CNF
-  maxVariable : Nat
 
 def assign (st : SolverState) (i : Nat) (v : Bool) : SolverState :=
   { st with
-      assignment := { map := st.assignment.map.insert i v },
+      assignment := st.assignment.insert i v,
       assignmentHistory := st.assignmentHistory.push i }
 
 def literalAssign (st : SolverState) : Literal -> SolverState
@@ -17,12 +17,12 @@ def literalAssign (st : SolverState) : Literal -> SolverState
 | Literal.neg i => assign st i false
 
 def isAssigned (st: SolverState) (i : Nat) : Bool :=
-  st.assignment.map.contains i
+  st.assignment.contains i
 
 def backtrack (st : SolverState) : SolverState :=
   let i := st.assignmentHistory.back
   { st with
-      assignment := { map := st.assignment.map.erase i },
+      assignment := st.assignment.erase i,
       assignmentHistory := st.assignmentHistory.pop }
 
 def currentState (st : SolverState) : Value :=
@@ -34,14 +34,14 @@ def litIndex : Literal -> Nat
 
 def freeLiterals (a : Assignment) (c : Clause) : Array Literal :=
   if evalClause a c = Value.sat then #[]
-  else Array.filter (a.map.contains ∘ litIndex) c.disjuncts
+  else Array.filter (a.contains ∘ litIndex) c
 
 def pickUnit : Array Literal -> Option Literal
 | #[u] => some u
 | _    => none
 
 def unitLiterals (a : Assignment) (c : CNF) : Array Literal :=
-  Array.filterMap (pickUnit ∘ freeLiterals a) c.conjucts
+  Array.filterMap (pickUnit ∘ freeLiterals a) c
 
 def assignUnitLiterals (st : SolverState) : SolverState :=
   let u := unitLiterals st.assignment st.cnf
@@ -72,7 +72,7 @@ def addClause (a : Assignment) (p : Purities) (c : Clause) : Purities :=
   Array.foldl addLiteral p $ freeLiterals a c
 
 def purities (a : Assignment) (c : CNF) : Purities :=
-  Array.foldl (addClause a) { purities := ∅ } c.conjucts
+  Array.foldl (addClause a) { purities := ∅ } c
 
 def pickPure : Nat × Purity -> Option Literal
 | ⟨n, Purity.pos⟩    => some (Literal.var n)
@@ -86,16 +86,30 @@ def assignPureLiterals (st : SolverState) : SolverState :=
   let p := pureLiterals $ purities st.assignment st.cnf
   Array.foldl literalAssign st p
 
-def nextVariable (cur : Nat) (st : SolverState) : Nat :=
-  let range := (List.range (st.maxVariable + 1)).drop (cur + 1)
-  (List.filter (not ∘ isAssigned st) range).headD 0
+def nextVariable (st : SolverState) : Nat :=
+  let maxv := Array.foldl max 0 st.assignmentHistory + 1
+  let pick := λ x y => if not $ st.assignment.contains x then x else y
+  Nat.fold pick maxv maxv
 
-def revertTo (n : Nat) (st : SolverState) : SolverState :=
-  match st.assignmentHistory.findIdx? (fun x => x = n) with
+def revertBranch (n : Nat) (st : SolverState) : SolverState :=
+  match st.assignmentHistory.findIdx? (λ x => x = n) with
   | none   => st
   | some i =>
-    let range := (List.range st.assignmentHistory.size).drop (i + 1)
+    let range := (List.range st.assignmentHistory.size).drop i
     List.foldl (fun s _ => backtrack s) st range
+
+def DPLLReduce : SolverState → SolverState :=
+  assignPureLiterals ∘ assignUnitLiterals
+
+def DPLLBranch (n : Nat) (v: Bool) (st : SolverState) : SolverState :=
+  let st' := assign st n v
+  { st' with branchHistory := st'.branchHistory.push n }
+  
+def DPLLRevert (st : SolverState) : Nat × Bool × SolverState :=
+  let n := st.branchHistory.back
+  let v := st.assignment.find! n
+  let st := {st with branchHistory := st.branchHistory.pop }
+  ⟨n, v, revertBranch n st⟩
 
 def DPLL : SolverState -> Nat -> SolverState × Value
 | st, 0 => ⟨st, Value.unknown⟩
@@ -104,31 +118,30 @@ def DPLL : SolverState -> Nat -> SolverState × Value
   | Value.sat => ⟨st, Value.sat⟩
   | Value.unsat => ⟨st, Value.unsat⟩
   | Value.unknown =>
-    let st := assignUnitLiterals st
-    let st := assignPureLiterals st
+    let st := DPLLReduce st
 
-    let cur := st.assignmentHistory.back
-    let next := nextVariable cur st
-    let st := assign st next true
+    let next := nextVariable st
+    let st := DPLLBranch next true st
     let ⟨st, val⟩ := DPLL st fuel
 
     if val = Value.unsat then
-      let st := revertTo cur st
+      let ⟨_, _, st⟩ := DPLLRevert st
       let st := assign st next false
       DPLL st fuel
     else
       ⟨st, Value.sat⟩
-      
+
 def maxVariableClause (c : Clause) :=
-  Array.foldl max 0 $ Array.map litIndex c.disjuncts
+  Array.foldl max 0 $ Array.map litIndex c
 
 def maxVariable (c : CNF) :=
-  Array.foldl max 0 $ Array.map maxVariableClause c.conjucts
+  Array.foldl max 0 $ Array.map maxVariableClause c
 
 def solveCNF (c : CNF) (fuel : Nat) : Value :=
   let st : SolverState := 
-    { assignment := { map := ∅ },
+    { assignment := (∅ : Std.HashMap _ _),
       assignmentHistory := ∅,
-      cnf := c,
-      maxVariable := maxVariable c }
+      branchHistory := ∅,
+      cnf := c, }
+
   (DPLL st fuel).snd
